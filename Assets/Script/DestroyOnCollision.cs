d204ec0cb1fd94cf84fa27b7915f3868fea9087e
ThanhTai <146913614+TaiNCT@users.noreply.github.com>
2024-05-25 17:37:57 +0700

    Finish

--- Assets/Script/DestroyOnCollision.cs
+++ Assets/Script/DestroyOnCollision.cs
@@ -5,8 +5,13 @@ public class DestroyOnCollision : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Solid")
+        if (other.tag == "Solid" || other.tag == "Enermy")
         {
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(1);
+            }
             Destroy(gameObject);
         }
     }

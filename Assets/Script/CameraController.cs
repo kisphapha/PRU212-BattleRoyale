c70284aebb61f30a6b625948ca67c374de8b9bb1
eleonora-vil <vinhthitheokaka@gmail.com>
2024-05-29 16:23:01 +0700

    update more guns

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera mainCamera;
+    public float zoomedOutSize = 10f;
+    public float normalSize = 5f;
+    public float zoomSpeed = 2f;
+
+    public void ZoomOut()
+    {
+        StartCoroutine(Zoom(zoomedOutSize));
+    }
+
+    public void ZoomIn()
+    {
+        StartCoroutine(Zoom(normalSize));
+    }
+
+    private IEnumerator Zoom(float targetSize)
+    {
+        while (Mathf.Abs(mainCamera.orthographicSize - targetSize) > 0.1f)
+        {
+            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, targetSize, Time.deltaTime * zoomSpeed);
+            yield return null;
+        }
+        mainCamera.orthographicSize = targetSize;
+    }
+}
